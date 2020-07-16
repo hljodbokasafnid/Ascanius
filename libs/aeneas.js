eeae3654a2e2ef6b83815f0011929946d4210246
Jóhann Sveinn Ingason <yohans89@gmail.com>
2020-07-16 13:16:51 +0000

    Multiple users dont share the same feed anymore
    Simply creating a room for each user on connection and emitting to that room handles this for us brilliantly.

--- libs/aeneas.js
+++ libs/aeneas.js
@@ -1,9 +1,12 @@
 const spawn = require("child_process").spawn;
+const { uuid } = require("uuidv4");
 
 exports = module.exports = function (io) {
   // Using the socket io, while connected if uploaded is called from client
   // Run the main script and emit the data to the server so we can read it client side
   io.sockets.on('connection', function (socket) {
+    var user_id = uuid();
+    socket.join(user_id);
     socket.on('uploaded', function (book_name) {
       var process = spawn('python3', ['./main.py', book_name]);
 
@@ -12,18 +15,18 @@ exports = module.exports = function (io) {
         // Refresh the page when the process is done and no error was raised
         // process.on exit couldn't differientiate between a clean exit and a forced exit
         if (new Buffer(data, 'utf-8').toString() == 'Done\n') {
-          var currentTime = new Date().toLocaleTimeString('en-GB');
-          io.emit('newdata', `${currentTime} - Refreshing.. Please Wait\n${currentTime} - Complete\n`);
-          io.emit('refresh');
+          var current_time = new Date().toLocaleTimeString('en-GB');
+          io.to(user_id).emit('newdata', `${current_time} - Refreshing.. Please Wait\n${current_time} - Complete\n`);
+          io.to(user_id).emit('refresh');
         } else {
-          io.emit('newdata', new Buffer(data, 'utf-8').toString());
+          io.to(user_id).emit('newdata', new Buffer(data, 'utf-8').toString());
         }
       });
 
       process.stderr.on('data', function (data) {
         // Errors also get relayed, in case of crashes. Also no refresh.
-        io.emit('newdata', new Buffer(data, 'utf-8').toString());
-        io.emit('error');
+        io.to(user_id).emit('newdata', new Buffer(data, 'utf-8').toString());
+        io.to(user_id).emit('error');
       });
     });
   });
--- libs/convert.js
+++ libs/convert.js
@@ -4,14 +4,11 @@ const path = require("path");
 const { uuid } = require("uuidv4");
 
 exports = module.exports = function (io) {
-  var web_sockets = {};
   // Using the socket io, while connected if uploaded is called from client
   // Run the main script and emit the data to the server so we can read it client side
   io.sockets.on('connection', function (socket) {
     var user_id = uuid();
-    web_sockets[user_id] = socket;
-    console.log(user_id);
-    console.log('added ' + user_id + ' to ' + Object.getOwnPropertyNames(web_sockets));
+    socket.join(user_id);
     socket.on('uploaded_convert', function (book_name) {
       var book_path = './public/uploads/' + book_name;
       var output_path = './public/';
@@ -25,26 +22,21 @@ exports = module.exports = function (io) {
         // Whenever SUCCESS is included in the data we conclude that we have finished and exit.
         var current_time = new Date().toLocaleTimeString('en-GB');
         if (new Buffer(data, 'utf-8').toString().includes("SUCCESS")) {
-          io.emit('newdata', `${current_time} - Refreshing.. Please Wait\n${current_time} - Complete\n`);
-          io.emit('refresh');
+          io.to(user_id).emit('newdata', `${current_time} - Refreshing.. Please Wait\n${current_time} - Complete\n`);
+          io.to(user_id).emit('refresh');
           // Clean up uploads folder for reupload and save space
           rimraf.sync(path.join(__dirname, '../public', 'uploads', book_name));
         } else if (new Buffer(data, 'utf-u').toString().includes("Error")) {
           // stderr.on does not work with the dp2.exe 
           // so if the data sent by stdout is includes error we can conclude that something didnt work
-          io.emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
-          io.emit('error');
+          io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
+          io.to(user_id).emit('error');
           // Clean up uploads folder for reupload and save space
           rimraf.sync(path.join(__dirname, '../public', 'uploads', book_name));
         } else {
-          io.emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
+          io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
         }
       });
-    });
-
-    socket.on('close', function () {
-      delete web_sockets[user_id];
-      console.log("deleted:", user_id);
     });
   });
 }
