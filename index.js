c54e6979d343babffee1d96ceac8ac8b29e22a40
Jóhann Sveinn Ingason <yohans89@gmail.com>
2020-07-17 08:54:43 +0000

    Version # Automatically Handled

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ const io = require('socket.io')(server);
 const aeneas = require('./libs/aeneas')(io)
 const convert = require('./libs/convert')(io)
 
+// Automatically create a version number based on repository
+var getVersion = require('git-repo-version');
+var version = getVersion({ shaLength: 0, includeDate: true });
+var date = new Date(version.split(" ")[1]).toLocaleString('en-GB');
+console.log("Running v" + version.split(" ")[0], "committed", date);
+
+// Listen to server  
 server.listen(listenPort, () => console.log(`App listening on port ${listenPort}`));
