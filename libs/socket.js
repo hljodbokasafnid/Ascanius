6f6eaadaee40f70891233e27c9c80406a1c4f758
Jóhann Sveinn Ingason <yohans89@gmail.com>
2020-08-11 14:35:40 +0000

    Update socket.js

--- libs/socket.js
+++ libs/socket.js
@@ -1,4 +1,5 @@
 const spawn = require("child_process").spawn;
+const spawnSync = require("child_process").spawnSync;
 const { v4: uuid } = require("uuid");
 
 const fs = require("fs-extra");
@@ -12,7 +13,7 @@ exports = module.exports = function (io) {
     var user_id = uuid();
     // Create a room for each user so each user gets their emitted separate messages
     socket.join(user_id);
-    
+
     // Aeneas creating smil files
     socket.on('uploaded', async function (folder_name, book_name) {
       // Copy contents to output before working on the original
@@ -20,12 +21,12 @@ exports = module.exports = function (io) {
       var destination = path.resolve(__dirname, "..", "public", "output", folder_name);
 
       await fs.copy(source, destination, function (err) {
-        if (err){
-            console.log('An error occured while copying the folder.')
-            return console.error(err);
+        if (err) {
+          console.log('An error occured while copying the folder.')
+          return console.error(err);
         }
       });
-      
+
       var process = spawn('python3', ['./main.py', folder_name, book_name]);
       process.stdout.on('data', function (data) {
         // Receive progress from aeneas script
@@ -54,21 +55,21 @@ exports = module.exports = function (io) {
     socket.on('uploaded_convert', async function (folder_name, book_name) {
       var book_path = './public/uploads/' + folder_name;
       var output_path = './public/';
-      
+
       var preprocess = spawn('python3', ['./scripts/preprocess.py', folder_name, book_name]);
-      preprocess.stdout.on('data', function(data) {
+      preprocess.stdout.on('data', function (data) {
         var current_time = new Date().toLocaleTimeString('en-GB');
         io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
       });
 
-      preprocess.stderr.on('data', function(data) {
+      preprocess.stderr.on('data', function (data) {
         var current_time = new Date().toLocaleTimeString('en-GB');
         io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
         io.to(user_id).emit('error');
       });
 
       // See list of optional commands here: https://daisy.github.io/pipeline/modules/daisy202-to-epub3/
-      var process = spawn('dp2.exe', ['daisy202-to-epub3', '--href', book_path + '/ncc.html', '--output', output_path, '--epub-filename', folder_name + '.epub', '-n', folder_name]);
+      var process = spawn('dp2', ['daisy202-to-epub3', '--href', book_path + '/ncc.html', '--output', output_path, '--epub-filename', folder_name + '.epub', '-n', folder_name]);
 
       process.stdout.on('data', function (data) {
         // Refresh the page when the process is done and no error was raised
@@ -98,51 +99,40 @@ exports = module.exports = function (io) {
       var books = await fs.readdir(book_path);
       var succeeded = [];
       var failed = [];
-      io.to(user_id).emit('newdata', `Conversion of ${parent_name} folder started.`);
-      const convertBooks = _ => {
-        books.forEach(async book => {
-          var book_name = await (async function() { 
-            var book_files = await fs.readdir(book_path + '/' + book);
-            for (var file in book_files) {
-              if (book_files[file].includes("html") && book_files[file] !== "ncc.html") {
-                // Relay the book name if aeneas
-                return book_files[file].split(".")[0];
-              }
+      io.to(user_id).emit('newdata', `Converting all daisy books found in '${parent_name}' folder.\n`);
+      for (var book in books) {
+        var book_name = await (async function () {
+          var book_files = await fs.readdir(book_path + '/' + books[book]);
+          for (var file in book_files) {
+            if (book_files[file].includes("html") && book_files[file] !== "ncc.html") {
+              // Relay the book name if aeneas
+              return book_files[file].split(".")[0];
             }
-          })();
-          var convertBook = await (async function() {
-            var preprocess = spawn('python3', ['./scripts/preprocess.py', parent_name + '/' + book, book_name]);
-            preprocess.stdout.on('data', function(data) {
-              var current_time = new Date().toLocaleTimeString('en-GB');
-              io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
-            });
-
-            preprocess.stderr.on('data', function(data) {
-              var current_time = new Date().toLocaleTimeString('en-GB');
-              io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
-              io.to(user_id).emit('error');
-            });
-            // See list of optional commands here: https://daisy.github.io/pipeline/modules/daisy202-to-epub3/
-            var process = await spawn('dp2.exe', ['daisy202-to-epub3', '--href', book_path + '/' + book + '/ncc.html', '--output', output_path, '--epub-filename', book + '.epub', '-n', book]);
-            process.stdout.on('data', function (data) {
-              console.log('any stdout');
-              // Refresh the page when the process is done and no error was raised
-              // Whenever SUCCESS is included in the data we conclude that we have finished and exit.
-              var current_time = new Date().toLocaleTimeString('en-GB');
-              if (new Buffer(data, 'utf-8').toString().includes("SUCCESS")) {
-                io.to(user_id).emit('newdata', `${current_time} - Refreshing.. Please Wait\n${current_time} - Complete\n`);
-              } else if (new Buffer(data, 'utf-8').toString().toLowerCase().includes("error")) {
-                // stderr.on does not work with the dp2.exe 
-                // so if the data sent by stdout is includes error we can conclude that something didnt work
-                io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
-              } else {
-                io.to(user_id).emit('newdata', `${current_time}: \n` + new Buffer(data, 'utf-8').toString() + "\n");
-              }
-            });
-          })();
-        });
+          }
+        })();
+        var current_time = new Date().toLocaleTimeString('en-GB');
+        io.to(user_id).emit('newdata', `${current_time}: \n[${Number(book) + 1}/${books.length}] - ${books[book]} Conversion Started\n`);
+        var preprocess = spawnSync('python3', ['./scripts/preprocess.py', parent_name + '/' + books[book], book_name]);
+        // See list of optional commands here: https://daisy.github.io/pipeline/modules/daisy202-to-epub3/
+        var process = spawnSync('dp2', ['daisy202-to-epub3', '--href', book_path + '/' + books[book] + '/ncc.html', '--output', output_path, '--epub-filename', books[book] + '.epub', '-n', books[book]]);
+        if (process.output.toString().includes('SUCCESS')) {
+          console.log("process string length:", process.output.toString().length);
+          var current_time = new Date().toLocaleTimeString('en-GB');
+          io.to(user_id).emit('newdata', `${current_time}: \n[${Number(book) + 1}/${books.length}] - ${books[book]} Conversion Succeeded\n`);
+          succeeded.push(books[book]);
+          console.log(succeeded);
+        } else {
+          var current_time = new Date().toLocaleTimeString('en-GB');
+          io.to(user_id).emit('newdata', `${current_time}: \n[${Number(book) + 1}/${books.length}] - ${books[book]} Conversion Failed\n`);
+          failed.push(books[book]);
+          console.log(failed);
+        }
       }
-      convertBooks();
+      var current_time = new Date().toLocaleTimeString('en-GB');
+      io.to(user_id).emit('newdata', `${current_time}: \n Succeeded: ${succeeded}\n Failed: ${failed}$`);
+      io.to(user_id).emit('newdata', `${current_time} - Refreshing.. Please Wait\n${current_time} - Complete\n`);
+      io.to(user_id).emit('refresh');
+      // TODO make get current time function
     });
   });
 }
