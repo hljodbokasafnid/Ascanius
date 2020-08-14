9cc3f4fa527d4900f03cd0a62a9c3d5e031090af
Jóhann Sveinn Ingason <yohans89@gmail.com>
2020-08-14 11:12:08 +0000

    Display Time Left for Upload
    Also Human Readable

--- public/javascript/upload.js
+++ public/javascript/upload.js
@@ -9,6 +9,7 @@ const description = document.querySelector('#upload-files .file-name');
 var current = window.location.pathname;
 
 fileInput.onchange = () => {
+  var timeStarted = new Date();
   $(".progress-bar").text("0%");
   $(".progress-bar").attr("value", 0);
   if (fileInput.files.length > 0) {
@@ -16,10 +17,12 @@ fileInput.onchange = () => {
     description.textContent = "Uploading Files..";
     var formData = new FormData();
     var bookname = undefined;
+
     // Relay the folder name
     var foldername = fileInput.files[0]['webkitRelativePath'].split("/")[0].split(" ").join("_");
     var uploadpath = '/upload/' + foldername;
     document.getElementById('upload-form').action = uploadpath;
+
     for (var i = 0; i < fileInput.files.length; i++) {
       // Check whether we are uploading for aeneas or dp2
       var filename = fileInput.files[i].name;
@@ -29,6 +32,7 @@ fileInput.onchange = () => {
       }
       formData.append('uploads', fileInput.files[i]);
     }
+
     $.ajax({
       url: uploadpath,
       type: "POST",
@@ -37,15 +41,21 @@ fileInput.onchange = () => {
       contentType: false,
       xhr: function () {
         var xhr = new XMLHttpRequest();
-
         xhr.upload.addEventListener("progress", function (evt) {
           if (evt.lengthComputable) {
+            // Get the estimated time left
             var percentComplete = evt.loaded / evt.total;
+            var uploadedBytes = evt.loaded;
+            var totalBytes = evt.total;
+            var timeElapsed = (new Date()) - timeStarted;
+            var uploadSpeed = uploadedBytes / (timeElapsed/1000);
+            var estimatedTimeLeft = (totalBytes - uploadedBytes) / uploadSpeed;
+            var timeString = new Date(1000 * Math.ceil(estimatedTimeLeft)).toISOString().substr(11, 8);
+            // Display the estimated time until upload finishes
+            $(".estimated-time-left").text("Estimated Time Left: " + timeString);
             $(".file-input").attr("disabled", true);
             percentComplete = parseInt(percentComplete * 100);
-
             $(".progress-bar").text(percentComplete + "%");
-            $(".progress-description").text("Uploading..");
             $(".progress-text").text(percentComplete + "%");
             $(".progress-bar").attr("value", percentComplete);
 
@@ -58,7 +68,7 @@ fileInput.onchange = () => {
                 description.textContent = "Batch Converting to epub3..";
               }
               $("#upload-files").attr("class", "file is-centered is-boxed is-info has-name is-large");
-              $(".progress-description").text("Upload Complete");
+              $(".estimated-time-left").text("Upload Complete");
               $("#file-label-span").text("Upload Complete");
               // Let server know that its uploaded and that the client expects data
               // Emit Uploaded for Aeneas, Uploaded_Convert for Conversion to Epub3, Uploaded_Convert_Batch for Batch Epub3 Conversion
